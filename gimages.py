46681239ada59f4ec3a3d4a7894e185e691413de
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-08-17 09:53:44 +0100

    Update gimages.py

--- gimages.py
+++ gimages.py
@@ -5,14 +5,26 @@
 from google_images_search import GoogleImagesSearch
 from dotenv import load_dotenv
 import os
+import sys
+
+# Once you've created them, save your API and CX in this format :
+# export DEVELOPER_KEY=QIzaSyD9G993bcg2049OxR0d9KOZNkjo8AdkMJc
+# export CX=9e25cccedd0ad4433
 
 # load the API key and CX code from .env file
 load_dotenv()
 
+# Check if search term provided
+if len(sys.argv) == 2:
+  searchfor = (sys.argv[1])
+else:
+    print("specify a search term eg: python3 gimages.py cat") 
+    sys.exit()
+
 # create an 'ims' sub directory if it doesn't already exists
-if not os.path.exists('ims/'):
-  os.mkdir('ims/')
-spath = 'ims/'
+if not os.path.exists('images/'):
+  os.mkdir('images/')
+spath = 'images/'
 
 # Get env variables
 DK = os.environ.get('DEVELOPER_KEY')
@@ -28,7 +40,7 @@ gis = GoogleImagesSearch(DK, CX, progressbar_fn=my_progressbar)
 # using contextual mode (Curses)
 with GoogleImagesSearch(DK, CX) as gis:
     # define search params:
-    _search_params = {"q": "bird", 
+    _search_params = {"q": searchfor, 
         "num": 10, 
         "safe": "high", 
         "fileType": "jpg",
@@ -39,4 +51,4 @@ with GoogleImagesSearch(DK, CX) as gis:
     gis.search(search_params=_search_params, path_to_dir=spath)
     gis.next_page()
 
-
+print("Done. Now check images folder")
